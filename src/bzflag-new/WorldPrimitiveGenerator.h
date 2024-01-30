f97c07d67135219b0d63fc6af7af6b1c14c28c85
bznext <>
2024-01-30 05:11:06 -0700

    Add planarPoly primitive generator

--- src/bzflag-new/WorldPrimitiveGenerator.h
+++ src/bzflag-new/WorldPrimitiveGenerator.h
@@ -1,6 +1,8 @@
 #ifndef WORLDPRIMITIVEGENERATOR_H
 #define WORLDPRIMITIVEGENERATOR_H
 
+#include <vector>
+
 #include <Magnum/Trade/MeshData.h>
 #include <Magnum/Math/Vector3.h>
 
@@ -10,6 +12,7 @@ class WorldPrimitiveGenerator {
 
         static Magnum::Trade::MeshData quad(const float base[3], const float uEdge[3], const float vEdge[3], float uOffset, float vOffset, float uRepeats, float vRepeats);
         static Magnum::Trade::MeshData tri(const float base[3], const float uEdge[3], const float vEdge[3], float uOffset, float vOffset, float uRepeats, float vRepeats);
+        static Magnum::Trade::MeshData planarPoly(std::vector<Magnum::Math::Vector3<float>> verts, std::vector<Magnum::Math::Vector3<float>> norms, std::vector<Magnum::Math::Vector3<float>> texcoords);
         static Magnum::Trade::MeshData debugLine(Magnum::Math::Vector3<float> a, Magnum::Math::Vector3<float> b);
 };
 
