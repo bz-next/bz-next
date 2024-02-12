59e43772b25536e42fab99d6a853120db8399201
bznext <>
2024-02-12 14:17:03 -0700

    Remove some unused drawables

--- src/gfx/include/Drawables.h
+++ src/gfx/include/Drawables.h
@@ -15,55 +15,6 @@
 
 #include "MagnumBZMaterial.h"
 
-class ColoredDrawable : public Magnum::SceneGraph::Drawable3D {
-    public:
-        explicit ColoredDrawable(Object3D& object, Magnum::Shaders::PhongGL& shader, Magnum::GL::Mesh& mesh, const Magnum::Color4& color, Magnum::SceneGraph::DrawableGroup3D& group) :
-            Magnum::SceneGraph::Drawable3D{object, &group},
-            _shader(shader),
-            _mesh(mesh),
-            _color(color)
-        {}
-
-    private:
-        void draw(const Magnum::Matrix4& transformationMatrix, Magnum::SceneGraph::Camera3D& camera) override;
-
-        Magnum::Shaders::PhongGL& _shader;
-        Magnum::GL::Mesh& _mesh;
-        Magnum::Color4 _color;
-};
-
-class InstancedColoredDrawable : public Magnum::SceneGraph::Drawable3D {
-    public:
-        explicit InstancedColoredDrawable(Object3D& object, Magnum::Shaders::PhongGL& shader, Magnum::GL::Mesh& mesh, Magnum::SceneGraph::DrawableGroup3D& group) :
-            Magnum::SceneGraph::Drawable3D{object, &group},
-            _shader(shader),
-            _mesh(mesh)
-        {}
-
-    private:
-        void draw(const Magnum::Matrix4& transformationMatrix, Magnum::SceneGraph::Camera3D& camera) override;
-
-        Magnum::Shaders::PhongGL& _shader;
-        Magnum::GL::Mesh& _mesh;
-};
-
-class TexturedDrawable : public Magnum::SceneGraph::Drawable3D {
-    public:
-        explicit TexturedDrawable(Object3D& object, Magnum::Shaders::PhongGL& shader, Magnum::GL::Mesh& mesh, Magnum::GL::Texture2D& texture, Magnum::SceneGraph::DrawableGroup3D& group) :
-            Magnum::SceneGraph::Drawable3D{object, &group},
-            _shader(shader),
-            _mesh(mesh),
-            _texture(texture)
-        {}
-
-    private:
-        void draw(const Magnum::Matrix4& transformationMatrix, Magnum::SceneGraph::Camera3D& camera) override;
-
-        Magnum::Shaders::PhongGL& _shader;
-        Magnum::GL::Mesh& _mesh;
-        Magnum::GL::Texture2D &_texture;
-};
-
 class BZMaterialDrawable : public Magnum::SceneGraph::Drawable3D {
     public:
         explicit BZMaterialDrawable(Object3D& object, Magnum::Shaders::PhongGL& shader, Magnum::Shaders::PhongGL& shaderUntex, Magnum::GL::Mesh& mesh, const MagnumBZMaterial* mptr, Magnum::SceneGraph::DrawableGroup3D& group) :
